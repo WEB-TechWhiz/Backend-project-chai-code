32e3d708c7f421c684d52e8f5a80d14107b507ef
rohit kumar <kurrohit300@gmail.com>
2025-08-19 11:07:47 +0530

    MongoDB connected

--- src/db/index.js
+++ src/db/index.js
@@ -1,49 +1,56 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
 
-// Load environment variables
-dotenv.config();
+// // Load environment variables
+// dotenv.config();
 
-const string = process.env.MONGODB_URI;
+// const string = process.env.MONGODB_URI;
 
-const connectionDB = async () => {
-  try {
-    if (!string) {
-      throw new Error("❌ MONGODB_URI is missing. Check your .env file.");
-    }
+// const connectionDB = async () => {
+//   try {
+//     if (!string) {
+//       throw new Error("❌ MONGODB_URI is missing. Check your .env file.");
+//     }
 
-    const connectionInstance = await mongoose.connect(string, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+//     const connectionInstance = await mongoose.connect(string, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
 
-    console.log("✅ MongoDB connected:", connectionInstance.connection.host);
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
-    process.exit(1);
-  }
-};
-
-export default connectionDB;
-
-import express from "express";
-// import connectionDB from "../db/index";
-
-const app = express();
-
-// Connect to MongoDB
-connectionDB();
-
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
-});
-
-
-
-
-
-
-
-
+//     console.log("✅ MongoDB connected:", connectionInstance.connection.host);
+//   } catch (error) {
+//     console.error("❌ MongoDB connection error:", error.message);
+//     process.exit(1);
+//   }
+// };
 
 // export default connectionDB;
+
+// import express from "express";
+// // import connectionDB from "../db/index";
+
+// const app = express();
+
+// // Connect to MongoDB
+// connectionDB();
+
+// app.listen(5000, () => {
+//   console.log("🚀 Server running on port 5000");
+// });
+// // export default connectionDB;
+
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../app/constants.js";
+
+const connectDB=async()=>{
+  try {
+     const connectionInstance=await mongoose.connect(`${process.env.MONGODB_USER}/${DB_NAME}`)
+     console.log(`\n MongoDB connected  !! DB HOST:${connectionInstance.connection.host}`);
+  } catch (error) {
+     console.log("MongoDB connection error",error)
+     process.exit(1)    
+  }
+}
+
+export default connectDB 
